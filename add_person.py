278f1ed8ab3c5c232db8d4fb955531f4c58c3d32
face_authentic <34530866+hossn555@users.noreply.github.com>
2019-09-11 10:53:00 +0900

    Update add_person.py

--- add_person.py
+++ add_person.py
@@ -2,14 +2,14 @@
 from face_authentic import config
 import cognitive_face as CF
 import glob
-KEY = setup.APIKEY
+KEY = config.APIKEY
 CF.Key.set(KEY)
-BASE_URL = setup.BASE_URL
+BASE_URL = config.BASE_URL
 CF.BaseUrl.set(BASE_URL)
 
 def add_person(name,pictures_path):
 
-	add_person = CF.person.create(setup.PERSON_GROUPID,name)
+	add_person = CF.person.create(config.PERSON_GROUPID,name)
 	personId = add_person["personId"]
 	if pictures_path[-1:] == "/":			#pathの最後が"/*"となるようにする
 		pictures_path = pictures_path + "*"
@@ -19,13 +19,11 @@ def add_person(name,pictures_path):
 	picture_list = glob.glob(pictures_path)
 	print("picture_list{}".format(picture_list))
 	for picture in picture_list:
-		result = CF.person.add_face(picture,setup.PERSON_GROUPID,personId)
+		result = CF.person.add_face(picture,config.PERSON_GROUPID,personId)
 		print(result)
-	CF.person_group.train(setup.PERSON_GROUPID) #学習させる
+	CF.person_group.train(config.PERSON_GROUPID) #学習させる
 
 if __name__ == '__main__':
 	name:str = input("Name: ")
 	pictures_path:str = input("Pictures directly path: ")
 	add_person(name,pictures_path)
-
-
